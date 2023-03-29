e241e82201792c97aba5b19e2e6fea2d41b8ff89
SamDaQueen <samreenreyaz@outlook.com>
2023-03-28 23:05:24 -0400

    Components using Redux; Like, add and delete Tuits

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,37 +1,43 @@
+import { configureStore } from "@reduxjs/toolkit";
 import React from "react";
+import { Provider } from "react-redux";
+import { Route, Routes } from "react-router";
+import Nav from "../nav";
 import ExploreComponent from "./explore";
 import HomeComponent from "./home";
 import NavigationSidebar from "./navigation-sidebar";
+import postsReducer from "./reducers/posts-reducer";
+import tuitsReducer from "./reducers/tuits-reducer";
+import whoReducer from "./reducers/who-reducer";
 import WhoToFollowList from "./who-to-follow-list";
-import { Route, Routes } from "react-router";
-import Nav from "../nav";
-import PostSummaryList from "./post-summary-list";
+
+const store = configureStore({
+  reducer: { who: whoReducer, tuits: tuitsReducer, posts: postsReducer },
+});
 
 function Tuiter() {
   return (
-    <div className="row mt-2">
-      <Nav />
-      <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-        <NavigationSidebar />
+    <Provider store={store}>
+      <div className="row mt-2">
+        <Nav />
+        <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+          <NavigationSidebar />
+        </div>
+        <div
+          className="col-10 col-md-10 col-lg-7 col-xl-6"
+          style={{ position: "relative" }}
+        >
+          <Routes>
+            <Route index element={<HomeComponent />} />
+            <Route path="home" element={<HomeComponent />} />
+            <Route path="explore" element={<ExploreComponent />} />
+          </Routes>
+        </div>
+        <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+          <WhoToFollowList />
+        </div>
       </div>
-      <div
-        className="col-10 col-md-10 col-lg-7 col-xl-6"
-        style={{ position: "relative" }}
-      >
-        <Routes>
-          <Route index element={<HomeComponent />} />
-          <Route path="home" element={<HomeComponent />} />
-          <Route path="explore" element={<ExploreComponent />} />
-        </Routes>
-      </div>
-      <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-        <Routes>
-          <Route index element={<PostSummaryList />} />
-          <Route path="home" element={<PostSummaryList />} />
-          <Route path="explore" element={<WhoToFollowList />} />
-        </Routes>
-      </div>
-    </div>
+    </Provider>
   );
 }
 
